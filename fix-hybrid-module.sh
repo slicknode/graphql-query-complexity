93d6520ebcc53dc1cc754e3d8bd0f4e4aaa8de44
Ivo Meißner <info@overtronic.com>
2024-09-05 22:03:41 -0500

    Fix NodeJS compatibility at runtime #93

--- fix-hybrid-module.sh
+++ fix-hybrid-module.sh
@@ -4,8 +4,12 @@ cat >dist/cjs/package.json <<!EOF
 }
 !EOF
 
+sed -i '' "s/require(\"graphql\/execution\/values\")/require(\"graphql\/execution\/values.js\")/" "dist/cjs/QueryComplexity.js"
+
 cat >dist/esm/package.json <<!EOF
 {
     "type": "module"
 }
 !EOF
+
+sed -i '' "s/from 'graphql\/execution\/values';/from 'graphql\/execution\/values.mjs';/" "dist/esm/QueryComplexity.js"
